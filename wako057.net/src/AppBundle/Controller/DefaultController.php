b738a2d08df2fb8e568c72446d67e45238f8bd49
Gregs <gregory.gerard@wister.fr>
2016-06-06 01:36:07 +0200

    routage dev ok

--- wako057.net/src/AppBundle/Controller/DefaultController.php
+++ wako057.net/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,10 +14,29 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-       
+        return $this->render('AppBundle:Default:index.html.twig');
+//        return new Response(
+//            '<html><body>Hello World</body></html>'
+//        );
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+//        ]);
+    }
+
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction(Request $request)
+    {
+        return $this->render('AppBundle:Default:about.html.twig');
+//        return new Response(
+//            '<html><body>Hello World</body></html>'
+//        );
+        // replace this example code with whatever you need
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+//        ]);
     }
 }
--- wako057.net/web/app.php
+++ wako057.net/web/app.php
@@ -9,7 +9,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
 //$kernel = new AppKernel('prod', false);
-$kernel = new AppKernel('dev', false);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
