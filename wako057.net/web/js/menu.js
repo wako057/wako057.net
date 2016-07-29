e5f9aa1dec835ffc9b73fdc1a08d0c821bd25fb3
Gregory Gerard <greg@gregs>
2016-07-29 17:09:22 +0200

    version 3 du menu

--- wako057.net/web/js/menu.js
+++ wako057.net/web/js/menu.js
@@ -1,15 +1,15 @@
 
-(function($){
-
-    /* Quand je clique sur l'icône hamburger je rajoute une classe au body */
-    $('#header__icon').click(function(e){
-        e.preventDefault();
-        $('body').toggleClass('with--sidebar');
-    });
-
-    /* Je veux pouvoir masquer le menu si on clique sur le cache */
-    $('#site-cache').click(function(e){
-        $('body').removeClass('with--sidebar');
-    })
-
-})(jQuery);
+// (function($){
+//
+//     /* Quand je clique sur l'icône hamburger je rajoute une classe au body */
+//     $('#header__icon').click(function(e){
+//         e.preventDefault();
+//         $('body').toggleClass('with--sidebar');
+//     });
+//
+//     /* Je veux pouvoir masquer le menu si on clique sur le cache */
+//     $('#site-cache').click(function(e){
+//         $('body').removeClass('with--sidebar');
+//     })
+//
+// })(jQuery);
