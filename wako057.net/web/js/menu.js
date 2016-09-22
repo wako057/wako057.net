ef2a48c761f6d99d64f945d854d9a0c7d5701e03
Gregs <gregory.gerard@wister.fr>
2016-09-22 22:07:39 +0200

    menu lateral Ok

--- wako057.net/web/js/menu.js
+++ wako057.net/web/js/menu.js
@@ -1,15 +1,14 @@
+(function($){
 
- (function($){
+    /* Quand je clique sur l'icône hamburger je rajoute une classe au body */
+    $('#header__icon').click(function(e){
+        e.preventDefault();
+        $('body').toggleClass('with--sidebar');
+    });
 
-     /* Quand je clique sur l'icône hamburger je rajoute une classe au body */
-     $('.logo').click(function(e){
-         e.preventDefault();
-         $('#lateral_nav').toggleClass('with--sidebar');
-     });
-//
-//     /* Je veux pouvoir masquer le menu si on clique sur le cache */
-//     $('#site-cache').click(function(e){
-//         $('body').removeClass('with--sidebar');
-//     })
-//
- })(jQuery);
+    /* Je veux pouvoir masquer le menu si on clique sur le cache */
+    $('#site-cache').click(function(e){
+        $('body').removeClass('with--sidebar');
+    })
+
+})(jQuery);
